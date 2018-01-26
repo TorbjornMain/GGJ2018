d20cb0eabe4f4276d8f486407368af8aa9f3177c
Unknown <a1688952@student.adelaide.edu.au>
2018-01-26 21:01:37 +1030

    Script folder added
    Added folder for scripts

---
+++ Assets/Scrips/Robot Movement/RoombaControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoombaControl : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            transform.Rotate(0, speed * Time.deltaTime, 0);
+        }
+
+    }
+    void FixedUpdate()
+    {
+
+    }
+}
