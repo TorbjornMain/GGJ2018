613594e7ca971b150cc6bf1fe7e9b2def412b6cd
Riley Huppatz <huprc001@mymail.unisa.edu.au>
2018-01-26 23:55:40 +1030

    Greyboxed a level and tweened a vent

---
+++ Assets/Scrips/Tweens/PressurePlate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour {
+    public GameObject vent;
+    void OpenVent()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Crate")
+        {
+            vent.SendMessage("Trigger");
+        }
+    }
+}
