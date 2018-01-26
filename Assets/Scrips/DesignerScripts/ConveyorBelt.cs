c80e13e85bb50b83137b7b7c885c4ab78e56122d
AIE\Toby <Tobykelty@gmail.com>
2018-01-27 00:22:31 +1030

    Conveyor belt added, set a dircetion and make sure that objects are tagged as Robots/Moveable if you want them to move

---
+++ Assets/Scrips/DesignerScripts/ConveyorBelt.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour {
+    public BoxCollider moveArea;
+    public Vector3 direction;
+    public float speed;
+    private Renderer rend;
+    private float offset;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag=="Robot" || other.tag == "Moveable")
+        {
+            other.gameObject.transform.Translate(direction*Time.deltaTime*speed);
+        }
+    }
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();    
+    }
+
+    private void Update()
+    {
+        offset += Time.deltaTime * speed;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
+}
