d838f6fd4d0e69e392fde96e68a97f7f18dd3ad8
Unknown <a1688952@student.adelaide.edu.au>
2018-01-26 21:47:30 +1030

    removed error

--- Assets/Scrips/Robot Movement/RoombaControl.cs
+++ Assets/Scrips/Robot Movement/RoombaControl.cs
@@ -4,20 +4,33 @@ using UnityEngine;
 
 public class RoombaControl : MonoBehaviour
 {
-
+    public float turnRate = 1;
+    Rigidbody rb;
+    public float moveSpeed;
+    public float fallSpeed;
+    bool grounded;
     // Use this for initialization
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxis("Horizontal") != 0)
+        RaycastHit hit;
+        rb.SweepTest(transform.forward, out hit, Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime);
+        if (!hit.collider)
         {
-            transform.Rotate(0, speed * Time.deltaTime, 0);
+            transform.Translate(transform.forward * Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime, Space.World);
+            transform.Rotate(0, Input.GetAxis("Horizontal") * turnRate * Time.deltaTime, 0);
         }
+        else
+        {
+
+        }
+
+
 
     }
     void FixedUpdate()
