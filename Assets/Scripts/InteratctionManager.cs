e5170c8a0cb2e68228dff63fda64e91beecb0f83
Riley Huppatz <huprc001@mymail.unisa.edu.au>
2018-02-02 14:43:25 +1030

    Remove some of the mp3s to shrink size and starting navmesh work

---
+++ Assets/Scripts/InteratctionManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteratctionManager : MonoBehaviour {
+    public GameObject StoredRobot;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0))
+        {
+            Ray toMouse = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit rhInfo;
+            bool didHit = Physics.Raycast(toMouse, out rhInfo, 500f);
+            if(didHit)
+            {
+                Debug.Log(rhInfo.collider.name);
+                rhInfo.collider.gameObject.SendMessage("Trigger");
+            }
+        }
+	}
+}
